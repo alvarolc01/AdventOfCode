40a8e483b93ef8ec40a5de642dbb6948c25df20e
alvarolc01 <alvaroluquecu@gmail.com>
2025-01-05 17:43:39 +0100

    feat: completed day 4 2016

---
+++ 2016/day_04/main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+const (
+	AlphabetSize   int = 26
+	PasswordLength int = 5
+)
+
+func readFile(fileName *string) []string {
+	file, err := os.Open(*fileName)
+	if err != nil {
+		fmt.Printf("Failed to open file: %s\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Failed to read file content: %s", err)
+		os.Exit(1)
+	}
+
+	return strings.Split(string(content), "\n")
+
+}
+
+func parseRoom(room string) (string, int, string) {
+	re := regexp.MustCompile(`^(.*)-(\d+)\[(.+)\]$`)
+	matches := re.FindStringSubmatch(room)
+
+	if matches == nil {
+		return "", 0, ""
+	}
+
+	encryptedName := matches[1]
+	sectorId, err := strconv.Atoi(matches[2])
+	if err != nil {
+		return "", 0, ""
+	}
+	checksum := matches[3]
+
+	return encryptedName, sectorId, checksum
+}
+
+func getFrequencyList(source string) map[rune]int {
+	frequencyList := make(map[rune]int)
+
+	for _, char := range source {
+		if unicode.IsLetter(char) {
+			frequencyList[char]++
+		}
+	}
+
+	return frequencyList
+}
+
+func sortFrequencies(frequencyList map[rune]int) []rune {
+	var sortedCounts []rune
+	for letter := range frequencyList {
+		sortedCounts = append(sortedCounts, letter)
+	}
+
+	sort.Slice(sortedCounts, func(i, j int) bool {
+		if frequencyList[sortedCounts[i]] == frequencyList[sortedCounts[j]] {
+			return sortedCounts[i] < sortedCounts[j]
+		}
+		return frequencyList[sortedCounts[i]] > frequencyList[sortedCounts[j]]
+	})
+
+	return sortedCounts
+}
+
+func getNMostFrequent(frequencyList map[rune]int) string {
+	sortedList := sortFrequencies(frequencyList)
+	password := ""
+
+	for i := 0; i < PasswordLength && i < len(sortedList); i++ {
+		password = password + string(sortedList[i])
+	}
+
+	return password
+}
+
+func isRoomReal(encryptedName, checksum string) bool {
+	if encryptedName == "" || checksum == "" {
+		return false
+	}
+	frequencyList := getFrequencyList(encryptedName)
+	obtainedPassword := getNMostFrequent(frequencyList)
+
+	return obtainedPassword == checksum
+}
+
+func decryptChar(char rune, shiftForward int) rune {
+	currentChar := int(char) - 'a'
+	shiftedChar := (currentChar + shiftForward) % AlphabetSize
+	return rune(shiftedChar + 'a')
+}
+
+func decryptRoom(encryptedName string, shiftForward int) string {
+	decryptedName := ""
+
+	for _, char := range encryptedName {
+		if char == '-' {
+			decryptedName = decryptedName + " "
+		} else {
+			decryptedName = decryptedName + string(decryptChar(char, shiftForward))
+		}
+	}
+
+	return decryptedName
+}
+
+func part1(input []string) {
+	sumSectorsID := 0
+
+	for _, room := range input {
+		encryptedName, sectorID, checksum := parseRoom(room)
+		if checksum != "" && isRoomReal(encryptedName, checksum) {
+			sumSectorsID += sectorID
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", sumSectorsID)
+}
+
+func part2(input []string) {
+	foundSectorId := 0
+
+	for _, room := range input {
+		encryptedName, sectorID, _ := parseRoom(room)
+		decryptedName := decryptRoom(encryptedName, sectorID)
+
+		if strings.Contains(strings.ToLower(decryptedName), "northpole") {
+			foundSectorId = sectorID
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", foundSectorId)
+}
+
+func main() {
+	fileName := flag.String("file", "", "Path to the file to read")
+	flag.Parse()
+
+	if *fileName == "" {
+		fmt.Println("File name not provided. Use --file to specify the file path.")
+		os.Exit(1)
+	}
+
+	inputStrings := readFile(fileName)
+
+	part1(inputStrings)
+	part2(inputStrings)
+
+}
