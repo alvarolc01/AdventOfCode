e5139914f073ef0ad2712f8a2ca13f9f15f69265
Alvaro Luque <alvaroluquecu@gmail.com>
2025-06-14 10:46:10 +0200

    feat: completed part 1

---
+++ 2024/day_01/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strings"
+)
+
+func part1(leftCol, rightCol []int) {
+	slices.Sort(leftCol)
+	slices.Sort(rightCol)
+
+	totalDistance := 0
+	for idx, val := range leftCol {
+		totalDistance += int(math.Abs(float64(val - rightCol[idx])))
+	}
+
+	fmt.Printf("Part 1: %d\n", totalDistance)
+}
+
+func parseInput(input string) (leftCol, rightCol []int, err error) {
+	lines := strings.Split(input, "\n")
+
+	leftCol = make([]int, len(lines))
+	rightCol = make([]int, len(lines))
+
+	for idx, line := range lines {
+		var leftNum, rightNum int
+		_, err = fmt.Sscanf(line, "%d\t%d", &leftNum, &rightNum)
+		if err != nil {
+			return
+		}
+
+		leftCol[idx] = leftNum
+		rightCol[idx] = rightNum
+	}
+
+	return
+}
+
+func main() {
+	fileName := flag.String("file", "", "Path to the file to read")
+	flag.Parse()
+
+	if *fileName == "" {
+		fmt.Println("file name not provided. Use --file to specify the file path.")
+		os.Exit(1)
+	}
+
+	fileContent, err := os.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("error reading input file:", err)
+		os.Exit(1)
+	}
+	input := string(fileContent)
+
+	leftCol, rightCol, err := parseInput(input)
+	if err != nil {
+		fmt.Println("error parsing input:", err)
+		os.Exit(1)
+	}
+
+	part1(leftCol, rightCol)
+
+}
