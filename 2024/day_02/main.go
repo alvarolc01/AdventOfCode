e0d86b71e172a36911e730aedc4dd45192fdd8b5
Alvaro Luque <alvaroluquecu@gmail.com>
2025-06-15 10:37:28 +0200

    feat: completed 2024 02 part1

---
+++ 2024/day_02/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func isReportSafe(report []int) bool {
+	if len(report) < 2 {
+		return true
+	}
+
+	isIncreasing := report[0] < report[1]
+
+	for idx := 1; idx < len(report); idx++ {
+		reportIncreasing := report[idx] > report[idx-1]
+		variation := int(math.Abs(float64(report[idx] - report[idx-1])))
+
+		if reportIncreasing != isIncreasing || variation < 1 || variation > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func part1(reports [][]int) {
+	safeReports := 0
+	for _, currReport := range reports {
+		if isReportSafe(currReport) {
+			safeReports++
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", safeReports)
+}
+
+func parseInput(input string) ([][]int, error) {
+	lines := strings.Split(input, "\n")
+	rows := make([][]int, len(lines))
+
+	for idx, line := range lines {
+
+		numbersLine := strings.Split(line, " ")
+		rows[idx] = make([]int, len(numbersLine))
+
+		for idxNum, num := range numbersLine {
+			convertedNum, err := strconv.Atoi(num)
+			if err != nil {
+				return nil, err
+			}
+
+			rows[idx][idxNum] = convertedNum
+		}
+
+	}
+
+	return rows, nil
+}
+
+func main() {
+	fileName := flag.String("file", "", "Path to the file to read")
+	flag.Parse()
+
+	if *fileName == "" {
+		fmt.Println("file name not provided. Use --file to specify the file path.")
+		os.Exit(1)
+	}
+
+	fileContent, err := os.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("error reading input file:", err)
+		os.Exit(1)
+	}
+	input := string(fileContent)
+
+	rows, err := parseInput(input)
+	if err != nil {
+		fmt.Println("error parsing input:", err)
+		os.Exit(1)
+	}
+
+	part1(rows)
+}
