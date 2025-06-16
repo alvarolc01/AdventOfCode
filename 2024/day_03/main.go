acb08da15e172f81779d710c6df25a2f63062dec
Alvaro Luque <alvaroluquecu@gmail.com>
2025-06-16 17:17:56 +0200

    feat: completed 2024 03 part 1

---
+++ 2024/day_03/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+)
+
+var (
+	reBase = regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)`)
+)
+
+func extractOperands(match string) (int, int, error) {
+	var a, b int
+	_, err := fmt.Sscanf(match, "mul(%d,%d)", &a, &b)
+	return a, b, err
+}
+
+func part1(rows []string) {
+	totalMult := 0
+
+	for _, line := range rows {
+		matches := reBase.FindAllString(line, -1)
+		for _, match := range matches {
+			multiplicand, multiplier, err := extractOperands(match)
+			if err != nil {
+				return
+			}
+			totalMult += multiplicand * multiplier
+		}
+	}
+
+	fmt.Printf("Part 1: %d\n", totalMult)
+}
+
+func main() {
+	fileName := flag.String("file", "", "Path to the file to read")
+	flag.Parse()
+
+	if *fileName == "" {
+		fmt.Println("file name not provided. Use --file to specify the file path.")
+		os.Exit(1)
+	}
+
+	fileContent, err := os.ReadFile(*fileName)
+	if err != nil {
+		fmt.Println("error reading input file:", err)
+		os.Exit(1)
+	}
+	rows := strings.Split(string(fileContent), "\n")
+
+	part1(rows)
+
+}
