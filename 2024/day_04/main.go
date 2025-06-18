abf519a73de9a7dae82dcbaec121f2b900323d39
Alvaro Luque <alvaroluquecu@gmail.com>
2025-06-18 11:07:58 +0200

    feat: completed 2024 04 part 2

--- 2024/day_04/main.go
+++ 2024/day_04/main.go
@@ -55,6 +55,34 @@ func part1(matrix []string) {
 	fmt.Printf("Part 1: %d\n", countApparitions)
 }
 
+func isMS(ch byte) bool {
+	return ch == 'M' || ch == 'S'
+}
+
+func isCrossedMax(matrix []string, row, col int) bool {
+	topLeft, topRight := matrix[row-1][col-1], matrix[row-1][col+1]
+	bottomLeft, bottomRight := matrix[row+1][col-1], matrix[row+1][col+1]
+
+	vertical := bottomLeft == topLeft && isMS(bottomLeft) && bottomRight == topRight && isMS(bottomRight) && bottomLeft != bottomRight
+	horizontal := topRight == topLeft && isMS(topRight) && bottomRight == bottomLeft && isMS(bottomLeft) && bottomLeft != topLeft
+
+	return vertical || horizontal
+}
+
+func part2(matrix []string) {
+	countApparitions := 0
+
+	for idxRow := 1; idxRow < len(matrix)-1; idxRow++ {
+		for idxCol := 1; idxCol < len(matrix[0])-1; idxCol++ {
+			if matrix[idxRow][idxCol] == 'A' && isCrossedMax(matrix, idxRow, idxCol) {
+				countApparitions++
+			}
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", countApparitions)
+}
+
 func main() {
 	fileName := flag.String("file", "", "Path to the file to read")
 	flag.Parse()
@@ -72,4 +100,5 @@ func main() {
 	rows := strings.Split(string(fileContent), "\n")
 
 	part1(rows)
+	part2(rows)
 }
