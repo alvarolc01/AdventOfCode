a986fa2e4fd295dfadb9c80859b241d2a1a31734
Alvaro Luque <alvaroluquecu@gmail.com>
2025-06-24 10:31:06 +0200

    feat: completed 2023 01 part 2

--- 2023/day_01/main.go
+++ 2023/day_01/main.go
@@ -8,6 +8,18 @@ import (
 	"unicode"
 )
 
+var wordsToNumbers = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
 func getCalibrationValues(line string) int {
 	first := -1
 	second := 0
@@ -35,6 +47,50 @@ func part1(input []string) {
 	fmt.Printf("Part 1: %d\n", sumCalibrationValues)
 }
 
+func extractNumberWord(line string, pos int) (int, bool) {
+	for key, val := range wordsToNumbers {
+		if strings.HasPrefix(line[pos:], key) {
+			return val, true
+		}
+	}
+
+	return 0, false
+}
+
+func getCalibrationValuesWithWords(line string) int {
+	first := -1
+	second := 0
+
+	for idx, char := range line {
+		currentDigit := -1
+		if unicode.IsNumber(char) {
+			currentDigit = int(char - '0')
+		} else {
+			number, found := extractNumberWord(line, idx)
+			if found {
+				currentDigit = number
+			}
+		}
+
+		if currentDigit != -1 {
+			second = currentDigit
+			if first == -1 {
+				first = currentDigit
+			}
+		}
+	}
+
+	return first*10 + second
+}
+
+func part2(input []string) {
+	sumCalibrationValues := 0
+	for _, row := range input {
+		sumCalibrationValues += getCalibrationValuesWithWords(row)
+	}
+	fmt.Printf("Part 2: %d\n", sumCalibrationValues)
+}
+
 func main() {
 	fileName := flag.String("file", "", "Path to the file to read")
 	flag.Parse()
@@ -52,4 +108,5 @@ func main() {
 	input := strings.Split(string(fileContent), "\n")
 
 	part1(input)
+	part2(input)
 }
